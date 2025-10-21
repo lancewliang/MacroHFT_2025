dd3c412082bba857bc3edcc47f1ffe939e45a5ac
lance.liang <78599194@qq.com>
2025-10-21 22:42:23 +0800

    代码优化

--- RL/agent/low_level.py
+++ RL/agent/low_level.py
@@ -100,7 +100,7 @@ parser.add_argument("--transcation_cost",type=float,default=2.0 / 10000)  # 交�
 parser.add_argument("--back_time_length",type=int,default=1)  # 历史窗口长度 / Historical window length
 parser.add_argument("--seed",type=int,default=12345)  # 随机种子 / Random seed
 parser.add_argument("--n_step",type=int,default=1)  # n-step TD目标 / N-step TD target
-parser.add_argument("--epoch_number",type=int,default=20)  # 训练轮次数 / Training epochs
+parser.add_argument("--epoch_number",type=int,default=30)  # 训练轮次数 / Training epochs
 parser.add_argument("--label",type=str,default="label_1")  # 标签列名称 / Label column name
 parser.add_argument("--clf",type=str,default="slope")  # 分类器类型 / Classifier type
 parser.add_argument("--alpha",type=float,default=4)  # KL损失权重系数 / KL loss weight coefficient
