14e425ff2b6eac11223cfc52bc5587efbc984048
lance.liang <78599194@qq.com>
2025-11-27 22:04:03 +0800

    123123

--- readme.md
+++ readme.md
@@ -21,10 +21,11 @@ Run scripts/high_level.sh for meta-policy optimization.
 
 After 4 months of unremitting efforts, mainly due to the lack of computing power equipment, I finally successfully reproduced the test results of low-level and high-level in your paper. Thank you for releasing all the code, algorithms, and data. Sincere tribute.
 I will continue to try to improve some parts of this framework, including:
---More actions, more position levels
---Due to the low trading frequency in my testing results, I tend to remove order data from the micro market for testing purposes
---Use LSTM or Transformer to observe the effect at the model level
+--More actions, more position levels  （已经测试过了， 不太可能成功，由于先验经验一定是满仓最大优势，导致学习不到半仓的好处，除非更换奖励函数为夏普之类的长期奖励，暂时不作研究）
+--Due to the low trading frequency in my testing results, （在没有持仓的时候增加了惩罚，交易频率上升了）
+--I tend to remove order data from the micro market for testing purposes
+--Use LSTM or Transformer to observe the effect at the model level 
 --Attempt to investigate the impact of ranbowdqn on the model
 --Incorporate spot prices into the factor,
---Short selling strategy, one-way short selling or as protection for long positions
---Test buying spot goods
+--Short selling strategy, one-way short selling or as protection for long positions （可行）
+--Test buying spot goods （放弃，由于现货交易的手续费较高0.3%，而我在测试中只关注了期货交易的收益，所以放弃了测试现货交易的效果，之后会考虑）
--- scripts/low_level.sh
+++ scripts/low_level.sh
@@ -1,9 +1,13 @@
 
 #nohup python -u RL/agent/low_level.py --alpha 1 --exp 'exp4_1' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --label label_2 >./logs/low_level/ETHUSDT/slope_1.log 2>&1 &
-nohup python -u RL/agent/low_level.py --alpha 1 --exp 'exp4_1' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0002 --action_mode both --label label_2 >./logs/low_level/ETHUSDT/slope_4_1.log 2>&1 &
-nohup python -u RL/agent/low_level.py --alpha 1 --exp 'exp4_2' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0004 --action_mode both --label label_2 >./logs/low_level/ETHUSDT/slope_4_2.log 2>&1 &
-nohup python -u RL/agent/low_level.py --alpha 1 --exp 'exp4_4' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0002 --action_mode long --label label_2 >./logs/low_level/ETHUSDT/slope_4_4.log 2>&1 &
-nohup python -u RL/agent/low_level.py --alpha 1 --exp 'exp4_3' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0002 --action_mode short --label label_2 >./logs/low_level/ETHUSDT/slope_4_3.log 2>&1 &
+# nohup python -u RL/agent/low_level.py --alpha 1 --exp 'exp4_1' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0002 --action_mode both --label label_2 >./logs/low_level/ETHUSDT/slope_4_1.log 2>&1 &
+nohup python -u RL/agent/low_level.py --alpha 1 --exp 'both_action' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0005 --action_mode both --action_size 1 --label label_2 >./logs/low_level/ETHUSDT/slope_4_1.log 2>&1 &
+# nohup python -u RL/agent/low_level.py --alpha 1 --exp 'long_action_0' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0002 --action_mode long --action_size 1 --label label_2 >./logs/low_level/ETHUSDT/slope_4_1.log 2>&1 &
+# nohup python -u RL/agent/low_level.py --alpha 1 --exp 'short_action_0' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0002 --action_mode short --action_size 1 --label label_2 >./logs/low_level/ETHUSDT/slope_4_2.log 2>&1 &
+# nohup python -u RL/agent/low_level.py --alpha 1 --exp 'long_action_1' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0004 --action_mode long --action_size 1 --label label_2 >./logs/low_level/ETHUSDT/slope_4_3.log 2>&1 &
+# nohup python -u RL/agent/low_level.py --alpha 1 --exp 'short_action_1' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0004 --action_mode short --action_size 1 --label label_2 >./logs/low_level/ETHUSDT/slope_4_4.log 2>&1 &
+nohup python -u RL/agent/low_level.py --alpha 1 --exp 'long_action' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0005 --action_mode long --action_size 1 --label label_2 >./logs/low_level/ETHUSDT/slope_4_5.log 2>&1 &
+nohup python -u RL/agent/low_level.py --alpha 1 --exp 'short_action' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --transcation_cost 0.0005 --action_mode short --action_size 1 --label label_2 >./logs/low_level/ETHUSDT/slope_4_6.log 2>&1 &
 #nohup python -u RL/agent/low_level.py --alpha 3 --exp 'exp4_3' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --label label_2 >./logs/low_level/ETHUSDT/slope_3.log 2>&1 &
 # nohup python -u RL/agent/low_level.py --alpha 4 --exp 'exp4_4' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --label label_1 >./logs/low_level/ETHUSDT/slope_4.log 2>&1 &
 # nohup python -u RL/agent/low_level.py --alpha 0 --exp 'exp4_0' --clf 'slope' --dataset 'ETHUSDT' --device 'cuda:0' --label label_1 >./logs/low_level/ETHUSDT/slope_0.log 2>&1 &
