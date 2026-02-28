263a44d97a3549a168408b1f18cf71b54bd0d502
lance.liang <78599194@qq.com>
2026-02-28 15:00:06 +0800

    sheel

--- scripts/both/MRMB/high_level_both_action_20260221.sh
+++ scripts/both/MRMB/high_level_both_action_20260221.sh
@@ -1,4 +1,6 @@
-#nohup python -u RL/agent/high_level.py --subagent_hidden_size=512 --buffer_size 2200000 --exp 'both_action_2023-2025_2_20260201' --dataset 'ETHUSDT' --transcation_cost 0.0005 --action_mode both --action_size 2 --reward_no_action False >./logs/high_level/ETHUSDT/both_action_2023-2025_2_20260201.log 2>&1 &
 
-nohup python -u RL/agent/high_level.py --subagent_hidden_size=512 --buffer_size 3000000 --exp 'both_action_2023-2025_2_20260201' --dataset 'ETHUSDT' --transcation_cost 0.0002 --action_mode both --action_size 2 --reward_no_action False >./logs/high_level/ETHUSDT/both_action_2023-2025_2_20260201.log 2>&1 &
+nohup python -u RL/agent/high_level.py --beta 2 --alpha 1 --lr 0.0001 --subagent_hidden_size=128 --buffer_size 1000000 --exp 'both_action_2_30s' --dataset 'MRMB' --transcation_cost 0.0002 --action_mode both --action_size 2 --reward_no_action False >./logs/high_level/MRMB/both_action_2_30s.log 2>&1 &
 
+
+
+nohup python -u RL/agent/high_level.py --beta 2 --alpha 1 --lr 0.0001 --subagent_hidden_size=128 --buffer_size 1000000 --exp 'both_action_5_30s' --dataset 'MRMB' --transcation_cost 0.0005 --action_mode both --action_size 2 --reward_no_action False >./logs/high_level/MRMB/both_action_5_30s.log 2>&1 &
--- scripts/copy_subagent_models.sh
+++ scripts/copy_subagent_models.sh
@@ -6,11 +6,13 @@
 #        <vol1_epoch>   <vol2_epoch>   <vol3_epoch>
 #
 # Example:
-#   ./scripts/copy_subagent_models.sh MRMB short short_action_5_1m 1 7 10 2 11 12 11   vol=11,vol2=12,vol3=11,slope1=7,slope2=10,slope3=2
-#   ./scripts/copy_subagent_models.sh MRMB short short_action_2_1m 1 11 2 13 7 14 11  slope1=11,slope2=2,slope3=13,vol1=7,vol2=14,vol3=11,
-#   ./scripts/copy_subagent_models.sh MRMB long long_action_2_1m 1 7 1 13 4 11 13  slope1=7,slope2=1,slope3=13,vol1=4,vol2=11,vol3=13,
-#   ./scripts/copy_subagent_models.sh MRMB short short_action_2_30s 1 10 14 6 14 14 10 slope1=10,slope2=14,slope3=6,vol1=14,vol2=14,vol3=10
-#   ./scripts/copy_subagent_models.sh MRMB both both_action_2_30s 1 5 8 7 11 11 11 slope1=5,slope2=8,slope3=7,vol1=11,vol2=11,vol3=11
+#   ./scripts/copy_subagent_models.sh MRMB short short_action_5_1m 1 7 10 2 11 12 11     vol=11,vol2=12,vol3=11,slope1=7,slope2=10,slope3=2
+#   ./scripts/copy_subagent_models.sh MRMB short short_action_2_1m 1 11 2 13 7 14 11     slope1=11,slope2=2,slope3=13,vol1=7,vol2=14,vol3=11,
+#   ./scripts/copy_subagent_models.sh MRMB long long_action_2_1m 1 7 1 13 4 11 13        slope1=7,slope2=1,slope3=13,vol1=4,vol2=11,vol3=13,
+#   ./scripts/copy_subagent_models.sh MRMB short short_action_2_30s 1 11 13 13 13 14 12  slope1=11,slope2=13,slope3=13,vol1=13,vol2=14,vol3=12
+#   ./scripts/copy_subagent_models.sh MRMB short short_action_5_30s 1 13 14 13 14 14 14  slope1=13,slope2=14,slope3=13,vol1=14,vol2=14,vol3=14
+#   ./scripts/copy_subagent_models.sh MRMB both both_action_2_30s 1 14 11 9 12 11 10     slope1=14,slope2=11,slope3=9,vol1=12,vol2=11,vol3=10
+#   ./scripts/copy_subagent_models.sh MRMB both both_action_5_30s 1 11 10 9 12 13 10     slope1=11,slope2=10,slope3=9,vol1=12,vol2=13,vol3=10
 
 
 
